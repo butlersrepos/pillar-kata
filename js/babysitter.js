79aed64d572c1e8a3c4e7920d84d51dfad4db0f0
Eric Butler <eric.butler@leadingedje.com>
2014-12-03 21:39:27 -0500

    Commit-5: Added implementations for post midnight and asleep hours calculations.

--- js/babysitter.js
+++ js/babysitter.js
@@ -5,19 +5,35 @@ Pillar.Babysitter = {
 	endTime                   : null,
 	bedTime                   : null,
 	calculatePostMidnightHours: function calculatePostMidnightHours() {
-		return -1;
+		if( !Pillar.Babysitter.startTime || !Pillar.Babysitter.endTime || !Pillar.Babysitter.bedTime ) {
+			console.debug( 'One of the time components is missing!' );
+			return 0;
+		}
+		var midnight = moment();
+		midnight.date( Pillar.Babysitter.startTime.date() + 1 );
+		midnight.hour( 0 ).minutes( 0 ).seconds( 0 ).milliseconds( 0 );
+
+		var hours = Pillar.Babysitter.endTime.diff( midnight, 'hours' );
+		return Math.max( 0, hours );
 	},
 	calculateAsleepHours      : function calculateAsleepHours() {
-		return -1;
+		if( !Pillar.Babysitter.startTime || !Pillar.Babysitter.endTime || !Pillar.Babysitter.bedTime ) {
+			console.debug( 'One of the time components is missing!' );
+			return 0;
+		}
+		var hours = Pillar.Babysitter.endTime.diff( Pillar.Babysitter.bedTime, 'hours' );
+
+		var postMidnightHours = Pillar.Babysitter.calculatePostMidnightHours();
+		hours -= postMidnightHours;
+		return Math.max( 0, hours );
 	},
 	calculateAwakeHours       : function calculateAwakeHours() {
 		if( !Pillar.Babysitter.startTime || !Pillar.Babysitter.endTime || !Pillar.Babysitter.bedTime ) {
-			console.debug( 'Either the start time or the end time are missing!' );
+			console.debug( 'One of the time components is missing!' );
 			return 0;
 		}
 		var hours = Pillar.Babysitter.bedTime.diff( Pillar.Babysitter.startTime, 'hours' );
 		return Math.max( 0, hours );
-
 	},
 	parseHours                : function parseHours() {
 		return {};
